f788c5e2ca920c71766aeef6343285bfc3d973b3
RadounaeBaba <radoine.baba04@gmail.com>
2024-04-25 16:38:29 +0000

    begin sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- create users table
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL AUTO_INCREMENT,
+	email varchar(255) NOT NULL
+	name varchar(255) NOT NULL
+	UNIQUE (email),
+	PRIMARY KEY (id));
