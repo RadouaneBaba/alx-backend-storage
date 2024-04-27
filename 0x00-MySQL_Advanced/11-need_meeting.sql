0338d9a899ec8ccc23ee9cd5ceede618d9820af2
RadounaeBaba <radoine.baba04@gmail.com>
2024-04-27 20:05:21 +0000

    correct

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,4 @@
 CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE score < 80
-AND (last_meeting is NULL OR last_meeting > ADDDATE(CURDATE(), INTERVAL -1 MONTH));
+AND (last_meeting is NULL OR last_meeting <= ADDDATE(CURDATE(), INTERVAL -1 MONTH));
