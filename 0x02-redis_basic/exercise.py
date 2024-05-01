a89d40e258cd086f13a27ae920b002902dd8e8db
RadouaneBaba <radoine.baba04@gmail.com>
2024-05-01 07:10:03 -0700

    convert

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Cache class implementation """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -16,3 +16,15 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable[[bytes], object]) -> object:
+        """ convert bytes to right type """
+        return fn(self._redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """ convert bytes to str """
+        return self.get(key, lambda x: str(x, 'utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ convert bytes to int """
+        return self.get(key, int)
