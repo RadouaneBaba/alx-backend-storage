e63ce1a488990da2440b946e980ac71f804aabcb
RadouaneBaba <radoine.baba04@gmail.com>
2024-05-01 08:32:31 -0700

    decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,17 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ count how many times method is called """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ increment count """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -11,6 +22,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store data in redis db """
         key = str(uuid.uuid4())
